bdda92f37e4ebf14e65e8780832eab9cc046ec65
Nelusa <nela.letochova@czechitas.cz>
2022-10-16 15:29:40 +0200

    První a druhá fáze

--- src/components/Stage/index.jsx
+++ src/components/Stage/index.jsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Wordbox from '../Wordbox';
 import wordList from '../../word-list';
 import './style.css';
 
 const generateWord = (size) => {
-  const sizeIndex = size === undefined
-    ? Math.floor(Math.random() * wordList.length)
-    : size - 3;
-  
+  const sizeIndex =
+    size === undefined ? Math.floor(Math.random() * wordList.length) : size - 3;
+
   if (sizeIndex < 0 || sizeIndex >= wordList.length) {
     return null;
   }
-  
+
   const words = wordList[sizeIndex];
   const wordIndex = Math.floor(Math.random() * words.length);
   return words[wordIndex];
@@ -20,11 +19,17 @@ const generateWord = (size) => {
 const Stage = () => {
   const [words, setWords] = useState(['jahoda']);
 
+  const handleFinish = () => {
+    setWords([generateWord(10)]);
+  };
+
   return (
     <div className="stage">
       <div className="stage__mistakes">Chyb: 0</div>
       <div className="stage__words">
-        {words.map((word) => <Wordbox word={word} key={word} />)}
+        {words.map((word) => (
+          <Wordbox word={word} key={word} onFinish={handleFinish} />
+        ))}
       </div>
     </div>
   );
--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
-  const [lettersLeft, setLettersLeft] = useState(word);  
-  
-  return (
-    <div className="wordbox">{lettersLeft}</div>
-  );
+const Wordbox = ({ word, onFinish }) => {
+  const [lettersLeft, setLettersLeft] = useState(word);
+
+  const [mistake, setMistake] = useState(false);
+  // (b) mistake && wordbox wordbox--mistake
+
+  const handleKeyUp = (e) => {
+    //console.log(e.key);
+    //console.log(lettersLeft);
+    //console.log(lettersLeft.substring(1));
+    if (lettersLeft[0] === e.key) {
+      if (lettersLeft.length > 1) {
+        setLettersLeft(lettersLeft.substring(1));
+      } else {
+        onFinish();
+      }
+      // setWords(words.slice(0, 1));
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleKeyUp);
+    return () => document.removeEventListener('keyup', handleKeyUp);
+  }, [lettersLeft]);
+
+  return <div className="wordbox">{lettersLeft}</div>;
 };
 
 export default Wordbox;
